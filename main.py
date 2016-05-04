c70280c6c5a80a0819d6d81c779f68cd8abd6748
Gilbert Montague <nerdgilbert@users.noreply.github.com>
2016-05-04 17:14:10 -0400

    Completed get action routine

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ class QAgent:
         self.cur_state, self.prev_state = 0;
         self.cur_action, self.prev_action = -1
 
+
     def get_box(self, observation):
         """
         The following routine was written by Rich Sutton and Chuck Anderson,
@@ -67,7 +68,7 @@ class QAgent:
         theta = observation[2]
         theta_dot = observation[3]
 
-        if( x < -2.4 || x > 2.4 || theta < -TWELVE_DEGREES || theta > TWELVE_DEGREES ):
+        if( x < -2.4 or x > 2.4 or theta < -TWELVE_DEGREES or theta > TWELVE_DEGREES ):
             return -1 #Signal a failure
 
         if( x < -0.8 ):
@@ -78,14 +79,14 @@ class QAgent:
             box = 2
 
         if(x_dot < -0.5):
-            #Do nothing
+            box = box
         elif(x_dot < 0.5):
             box += 3
         else:
             box += 6
 
         if(theta < -SIX_DEGREES):
-            #Do nothing
+            box = box
         elif(theta < -ONE_DEGREE):
             box +=9
         elif(theta < 0):
@@ -98,7 +99,7 @@ class QAgent:
             box += 45
 
         if( theta_dot < -FIFTY_DEGREES):
-            #Do nothing
+            box = box
         elif( theta_dot < FIFTY_DEGREES):
             box += 54
         else:
@@ -108,17 +109,40 @@ class QAgent:
 
 
     def get_action(self, observation, reward):
-        return 0
 
+        predicted_value = 0.0
 
+        if( self.first_time ):
+            self.first_time = False
+            self.reset_controller()
 
+            self.q_val.fill(self.W_INIT)
 
+            self.print_controller_information()
 
+        self.prev_state = self.cur_state
+        self.prev_action = self.cur_action
+
+        self.cur_state = self.get_box( observation )
+
+        if( self.prev_action != -1 ): #Update
+            if( self.cur_state == -1 ):
+                predicted_value = 0.0
+            elif(self.q_val[self.cur_state][0] <= self.q_val[self.cur_state][1]):
+                predicted_value = self.q_val[self.cur_state][1]
+            else:
+                predicted_value = self.q_val[self.cur_state][0]
+
+            self.q_val[self.prev_state][self.prev_action] += self.ALPHA * (reward + self.GAMMA * predicted_value - self.q_val[self.prev_state][self.prev_action])
+
+        if( self.q_val[self.cur_state][0] + random.uniform(-self.BETA, self.BETA) <= self.q_val[self.cur_state][1] ):
+            self.cur_action = 1
+        else:
+            self.cur_action = 0
+
+        return self.cur_action
 
 
 
 agent = QAgent()
-agent.print_controller_information()
-
-#env = gym.make('CartPole-v0')
 
